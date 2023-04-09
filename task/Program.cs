7be29c2e764d37888da4f801b919892cf537bf63
Your Name <dmitriirod@yandex.ru>
2023-04-10 00:41:58 +0300

    Added a solution the task

--- task/Program.cs
+++ task/Program.cs
@@ -4,6 +4,9 @@ int arraySize = Convert.ToInt32(Console.ReadLine());
 
 string[] createArrayStrings = CreateArrayStrings(arraySize);
 PrintArray(createArrayStrings);
+int sizeNewArray = SizeNewArray(createArrayStrings, 3);
+string[] newArrayStringsLessThanOrEqual = NewArrayStringsLessThanOrEqual(createArrayStrings, sizeNewArray);
+PrintArray(newArrayStringsLessThanOrEqual);
 
 
 string[] CreateArrayStrings(int size)
@@ -25,8 +28,44 @@ void PrintArray(string[] arr)
         if (i < arr.Length - 1) Console.Write($"“{arr[i]}”, ");
         else Console.Write($"“{arr[i]}”");
     }
-    Console.Write("]");
+    Console.WriteLine("]");
 }
 
+int SizeNewArray(string[] str, int numberCharacters)
+{
+    int sizeArr = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        int count = 0;
+        for (int y = 0; y < str[i].Length; y++)
+        {
+            count++;
+        }
+        if (count <= numberCharacters)
+        {
+            sizeArr++;
+        }
+    }
+    return sizeArr;
 
+}
 
+string[] NewArrayStringsLessThanOrEqual(string[] str, int sizeArr)
+{
+    string[] arr = new string[sizeArr];
+    int j = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        int count = 0;
+        for (int y = 0; y < str[i].Length; y++)
+        {
+            count++;
+        }
+        if (count <= 3)
+        {
+            arr[j] = str[i];
+            j++;
+        }
+    }
+    return arr;
+}
